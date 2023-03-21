09feb506dfa98d2f68075bb0494ecad195c7c8f1
Sasha <svasilenko389@gmail.com>
2023-03-22 01:31:54 +0200

    init

--- src/Components.js
+++ src/Components.js
@@ -1,7 +1,7 @@
 export function NavBar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-primary ">
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <nav className="navbar navbar-collapse navbar-expand-lg navbar-light bg-primary navbar-inverse">
+            <div className="container-fluid collapse navbar-collapse" id="navbarTogglerDemo02">
                 <ul className="navbar-nav mr-auto mt-1 ms-lg-4">
                     <li className="nav-item active">
                         <a className="navbar-brand text-white" href="/">Home</a>
@@ -14,7 +14,7 @@ export function NavBar() {
                     </li>
                 </ul>
             </div>
-            <div className="justify-content-end collapse navbar-collapse" id="navbarTogglerDemo03">
+            <div className=" container-fluid justify-content-end collapse navbar-collapse" id="navbarTogglerDemo03">
                 <ul className="navbar-nav mr-auto mt-1 ms-lg-4">
                     <li className="nav-item d-flex">
                         <a className="navbar-brand text-white" href="/login">Login</a>
