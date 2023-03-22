49b6d5a41916cba99829d679d386d690548ddb2d
Sasha <svasilenko389@gmail.com>
2023-03-22 12:07:45 +0200

    add hash

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import {App} from "./App";
-import { BrowserRouter } from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
     <StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>
 );
 
